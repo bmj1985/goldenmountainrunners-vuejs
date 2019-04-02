0abffe80c869a7bc96dc3fbf79aae387c5ff3afa
Brandon Johnson <bmj1985@gmail.com>
2019-04-01 19:47:41 -0600

    fleshing out basics of homepage

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <div id="nav">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>
+      </div>
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer/>
   </div>
 </template>
 
 <style lang="scss">
+@import "reset";
+@import "colors";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+  header {
+    height: 7vh;
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: $primary-0;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    z-index: 10;
+  }
+
+  main {
+    z-index: -99999;
+    height: 88vh;
+    width: 100vw;
+    background-image: url("./assets/goldentopo.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    /* margin-top: 10px;
+    margin-bottom: 10px; */
+  }
+
+  footer {
+    height: 5vh;
+    width: 100vw;
+    background: $primary-0;
+    box-shadow: 0 -10px 20px rgba(0, 0, 0, 0.19), 0 -6px 6px rgba(0, 0, 0, 0.23);
+    z-index: 1;
   }
 }
 </style>
---
+++ src/components/RunDescription.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="run-description"
+  >This week we will run from <a href="http://www.mountaintoadbrewing.com" target="_blank">Mountain Toad Brewing</a> up North Table. The run is a classic out and back that reaches the highest point on North Table (Lichen Peak) and is one of the <a href="http://www.goldenhellweek.com" target="_blank">2019 Golden Hell Week</a> routes. The total distance is just over 4.5 miles. After the run we will celebrate our activity with happy hour priced beers at Mountain Toad. See you Tuesday!</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "RunDescription",
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.run-description {
+    font-size: 1.5rem;
+    text-align: justify;
+    line-height: 1.1;
+}
+a {
+  text-decoration: none;
+}
+</style>
