49d7c1c0e393ab4003b4a6bc0f7050739732d976
Brandon Johnson <bmj1985@gmail.com>
2019-04-03 10:11:21 -0600

    add social media icons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
     <header>
-      <div id="nav">
+      <!-- <div id="nav">
         <router-link to="/">Home</router-link>|
         <router-link to="/about">About</router-link>
-      </div>
+      </div> -->
     </header>
     <main>
       <router-view/>
     </main>
-    <footer/>
+      <footer>
+      <a href="https://www.facebook.com/groups/lookoutmountainrunners" target="_blank">
+        <i class="fab fa-facebook-square fa-2x"></i>
+      </a>
+      <a href="https://www.instagram.com/goldenmountainrunners" target="_blank">
+        <i class="fab fa-instagram fa-2x"></i>
+      </a>
+    </footer>
   </div>
 </template>
 
@@ -53,6 +60,16 @@
     background: $primary-0;
     box-shadow: 0 -10px 20px rgba(0, 0, 0, 0.19), 0 -6px 6px rgba(0, 0, 0, 0.23);
     z-index: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    a {
+      text-decoration: none;
+      i {
+        margin: 1rem;
+        color: $gmr-grey;
+      }
+    }
   }
 }
 </style>
